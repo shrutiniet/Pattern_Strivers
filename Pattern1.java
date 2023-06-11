cacdd11b122ba08d7227763321857c229c548439
Shruti Sharma <69854848+shrutiniet@users.noreply.github.com>
2023-06-11 14:05:05 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+package Pattern;
+import java.util.*;
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
